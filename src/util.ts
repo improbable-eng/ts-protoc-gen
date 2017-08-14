d9a13de3d5851f57d0d522c3718a15f41d3bb345
Jonny Reeves <github@jonnyreeves.co.uk>
2017-08-14 20:41:28 +0100

    Add `pb_` prefix to JS Keywords (fixes #19)
    Matches the logic found in protobuf/compiler/js/js_generator.cc

--- src/util.ts
+++ src/util.ts
@@ -64,3 +64,70 @@ export function withAllStdIn(callback: (buffer: Buffer) => void): void {
     callback(Buffer.concat(ret, len));
   });
 }
+
+// normaliseFieldObjectName modifies the field name that appears in the `asObject` representation
+// to match the logic found in `protobuf/compiler/js/js_generator.cc`. See: https://goo.gl/tX1dPQ
+export function normaliseFieldObjectName(name: string): string {
+  switch (name) {
+    case "abstract":
+    case "boolean":
+    case "break":
+    case "byte":
+    case "case":
+    case "catch":
+    case "char":
+    case "class":
+    case "const":
+    case "continue":
+    case "debugger":
+    case "default":
+    case "delete":
+    case "do":
+    case "double":
+    case "else":
+    case "enum":
+    case "export":
+    case "extends":
+    case "false":
+    case "final":
+    case "finally":
+    case "float":
+    case "for":
+    case "function":
+    case "goto":
+    case "if":
+    case "implements":
+    case "import":
+    case "in":
+    case "instanceof":
+    case "int":
+    case "interface":
+    case "long":
+    case "native":
+    case "new":
+    case "null":
+    case "package":
+    case "private":
+    case "protected":
+    case "public":
+    case "return":
+    case "short":
+    case "static":
+    case "super":
+    case "switch":
+    case "synchronized":
+    case "this":
+    case "throw":
+    case "throws":
+    case "transient":
+    case "try":
+    case "typeof":
+    case "var":
+    case "void":
+    case "volatile":
+    case "while":
+    case "with":
+      return `pb_${name}`;
+  }
+  return name;
+}
