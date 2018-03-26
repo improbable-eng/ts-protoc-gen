07f1305d127cd0297189c854277b8f5ce39930ac
Jonny Reeves <john.reeves@improbable.io>
2018-03-26 13:26:29 +0100

    Create JavaScript sources and TypeScript definitions for grpc-web services (#44)
    * Create JavaScript sources and TypeScript definitions when generating grpc-web Services
    
    This ensures that the final output is valid ES5 and doesn't require further transpiling; essential if you want your generated code to live in its own package and therefore not require further transpiling from TypeScript to JavaScript.
    
    * Remove legacy js service code and references.
    
    * Simplify filenames and remove references to old bins
    
    * Remove errant comma

--- test/ts_test/src/service.ts
+++ test/ts_test/src/service.ts
@@ -1,6 +1,7 @@
 import {resolve} from "path";
 import {readFileSync} from "fs";
 import {assert} from "chai";
+import { createContext, runInContext } from "vm";
 import * as simple_service_pb_service from "../generated/examplecom/simple_service_pb_service";
 import * as simple_service_pb from "../generated/examplecom/simple_service_pb";
 import * as external_child_message_pb from "../generated/othercom/external_child_message_pb";
@@ -24,10 +25,56 @@ describe("ts service", () => {
     assert.strictEqual(simple_service_pb_service.SimpleService.DoStream.responseType, external_child_message_pb.ExternalChildMessage);
   });
   it("should not output imports for namespaces that are not used in the service definition", () => {
-    const generatedService = readFileSync(resolve(__dirname, "../../generated/examplecom/simple_service_pb_service.ts"), "utf8");
+    const generatedService = readFileSync(resolve(__dirname, "../../generated/examplecom/simple_service_pb_service.d.ts"), "utf8");
     assert.notInclude(generatedService, "google-protobuf/google/protobuf/timestamp_pb");
 
     const generatedProto = readFileSync(resolve(__dirname, "../../generated/examplecom/simple_service_pb.js"), "utf8");
     assert.include(generatedProto, "google-protobuf/google/protobuf/timestamp_pb");
+  });
+  it("should generate valid javascript sources", () => {
+    const generatedService = readFileSync(resolve(__dirname, "../../generated/examplecom/simple_service_pb_service.js"), "utf8");
+
+    // Create a sandbox into which the javascript module will be exported.
+    const sandbox = { exports: { SimpleService: simple_service_pb_service.SimpleService } };
+    createContext(sandbox);
+
+    // Create a wrapper around the module's source code which injects a stubbed require function
+    // which will return stub modules.
+    const wrapper = `((require) => { ${generatedService} });`;
+    const fakeRequire = (name: string) => {
+      if (name.indexOf("/simple_service_pb") !== -1) {
+        return {
+          UnaryRequest: simple_service_pb.UnaryRequest,
+          StreamRequest: simple_service_pb.StreamRequest,
+        }
+      }
+      else if (name.indexOf("/external_child_message_pb") !== -1) {
+        return {
+          ExternalChildMessage: external_child_message_pb.ExternalChildMessage,
+        }
+      }
+      return null;
+    };
+
+    runInContext(wrapper, sandbox)(fakeRequire);
+
+    assert.strictEqual(typeof sandbox.exports.SimpleService, "function");
+    assert.strictEqual(sandbox.exports.SimpleService.serviceName, "examplecom.SimpleService");
+
+    assert.strictEqual(typeof sandbox.exports.SimpleService.DoStream, "function");
+    assert.strictEqual(sandbox.exports.SimpleService.DoStream.methodName, "DoStream");
+    assert.strictEqual(sandbox.exports.SimpleService.DoStream.service, sandbox.exports.SimpleService);
+    assert.strictEqual(sandbox.exports.SimpleService.DoStream.requestStream, false);
+    assert.strictEqual(sandbox.exports.SimpleService.DoStream.responseStream, true);
+    assert.strictEqual(sandbox.exports.SimpleService.DoStream.requestType, simple_service_pb.StreamRequest);
+    assert.strictEqual(sandbox.exports.SimpleService.DoStream.responseType, external_child_message_pb.ExternalChildMessage);
+
+    assert.strictEqual(typeof sandbox.exports.SimpleService.DoUnary, "function");
+    assert.strictEqual(sandbox.exports.SimpleService.DoUnary.methodName, "DoUnary");
+    assert.strictEqual(sandbox.exports.SimpleService.DoUnary.service, sandbox.exports.SimpleService);
+    assert.strictEqual(sandbox.exports.SimpleService.DoUnary.requestStream, false);
+    assert.strictEqual(sandbox.exports.SimpleService.DoUnary.responseStream, false);
+    assert.strictEqual(sandbox.exports.SimpleService.DoUnary.requestType, simple_service_pb.UnaryRequest);
+    assert.strictEqual(sandbox.exports.SimpleService.DoUnary.responseType, external_child_message_pb.ExternalChildMessage);
   });
 });
