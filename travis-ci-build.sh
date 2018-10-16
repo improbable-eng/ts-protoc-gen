1b955f4cd586936edc82a83bca33b001387f6546
Jonny Reeves <jonny@improbable.io>
2018-10-16 12:26:42 +0100

    CI should check that generated code has been committed
    Add a travis build step which re-generates the generated code and then checks for changes to the workspce. If any are detected it will fail the build and prompt the user to run the `./generate.sh` script.

--- travis-ci-build.sh
+++ travis-ci-build.sh
@@ -3,4 +3,12 @@ set -ex
 
 npm run lint
 npm test
+
+./generate.sh
+MODIFIED_FILES=$(git diff --name-only)
+if [[ -n $MODIFIED_FILES ]]; then
+  echo "ERROR: Changes detected in generated code, please run './generate.sh' and check-in the changes."
+  exit 1
+fi
+
 bazel build //...:all
