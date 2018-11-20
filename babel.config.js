885f2c63c1fa8903b410dc50eeda129f0a2f4490
Chris Gervang <chr@uber.com>
2018-11-19 21:24:22 -0800

    try fix build

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 // used for flow type stripping and
 module.exports = function(api) {
-  api.cache(true);
+  api.cache(false);
   return {
     presets: ["@babel/preset-env", "@babel/preset-flow"]
   };
