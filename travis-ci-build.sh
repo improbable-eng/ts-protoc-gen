18ec0a0536077f57d78c16b9a12d8b0f6ce61619
Chris Gervang <chr@uber.com>
2018-11-19 22:22:30 -0800

    Update travis-ci-build.sh

--- travis-ci-build.sh
+++ travis-ci-build.sh
@@ -5,7 +5,7 @@ npm run lint
 npm test
 
 ./generate.sh
-MODIFIED_FILES=$(git diff)
+MODIFIED_FILES=$(git diff --name-only)
 if [[ -n $MODIFIED_FILES ]]; then
   echo "ERROR: Changes detected in generated code, please run './generate.sh' and check-in the changes."
   exit 1
