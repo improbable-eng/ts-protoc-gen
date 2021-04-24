fd6e3a06bea3f5e2c6fb61f140ca6ba2ea46871e
Marcus Longmuir <marcus.longmuir@improbable.io>
2021-04-24 09:59:30 +0100

    Fix CI failing due to diff caused by npm install (#277)
    * Printing git diff
    
    * Stop caching node_modules

--- travis-ci-build.sh
+++ travis-ci-build.sh
@@ -5,6 +5,9 @@ npm run lint
 npm test
 
 npm run generate
+
+git --no-pager diff
+
 MODIFIED_FILES=$(git diff --name-only)
 if [[ -n $MODIFIED_FILES ]]; then
   echo "ERROR: Changes detected in generated code, please run 'npm run generate' and check-in the changes."
